915b6c5c8f780894fac88ff00c537f9aa218fb8a
PSPadJSHint <pspadjshint@gmail.com>
2013-07-15 09:55:36 +0200

    Version 3.1.1. More (default) options.

--- PSPadJSHint.js
+++ PSPadJSHint.js
@@ -40,7 +40,7 @@ JSHINT
 
 var menuName = "JSHint",
     module_name = "PSPadJSHint",
-    module_ver = "1.1.1",
+    module_ver = "3.1.1",
     myself = this;
 
 
@@ -563,45 +563,62 @@ function jSHintAllOpenFilesClose() {
     bitwise     : true, // if bitwise operators should not be allowed
     boss        : false, // if advanced usage of assignments should be allowed
     browser     : true, // if the standard browser globals should be predefined
+    camelcase   : false, // if identifiers should be required in camel case
     couch       : false, // if CouchDB globals should be predefined
     curly       : false, // if curly braces around blocks should be required (even in if/for/while)
     debug       : false, // if debugger statements should be allowed
     devel       : false, // if logging globals should be predefined (console, alert, etc.)
+    dojo        : false, // if Dojo Toolkit globals should be predefined
     eqeqeq      : true, // if === should be required
     eqnull      : false, // if == null comparisons should be tolerated
     es5         : true, // if ES5 syntax should be allowed
+    esnext      : true, // if es.next specific syntax should be allowed
     evil        : false, // if eval should be allowed
     expr        : false, // if ExpressionStatement should be allowed as Programs
     forin       : true, // if for in statements must filter
+    funcscope   : true, // if only function scope should be used for scope tests
     globalstrict: false, // if global "use strict"; should be allowed (also enables 'strict')
     immed       : true, // if immediate invocations must be wrapped in parens
+    iterator    : true, // if the `__iterator__` property should be allowed
     jquery      : false, // if jQuery globals should be predefined
+    lastsemic   : false, // if semicolons may be ommitted for the trailing statements inside of a one-line blocks.
     latedef     : true, // if the use before definition should not be tolerated
     laxbreak    : false, // if line breaks should not be checked
+    laxcomma    : true, // if line breaks should not be checked around commas
     loopfunc    : false, // if functions should be allowed to be defined within loops
     mootools    : true, // if MooTools globals should be predefined
+    multistr    : true, // allow multiline strings
     newcap      : true, // if constructor names must be capitalized
     noarg       : true, // if arguments.caller and arguments.callee should be disallowed
     node        : true, // if the Node.js environment globals should be predefined
     noempty     : true, // if empty blocks should be disallowed
-    nomen       : true, // if names should be checked
+    nomen       : false,// if names should be checked
     nonew       : true, // if using `new` for side-effects should be disallowed
+    nonstandard : false, // if non-standard (but widely adopted) globals should be predefined
     onevar      : true, // if only one var statement per function should be allowed
+    onecase     : true, // if one case switch statements should be allowed
     passfail    : false, // if the scan should stop on first error
     plusplus    : false, // if increment/decrement should not be allowed
+    proto       : true, // if the `__proto__` property should be allowed
     prototypejs : false, // if Prototype and Scriptaculous globals should be predefined
     regexdash   : true, // if unescaped last dash (-) inside brackets should be tolerated
     regexp      : false, // if the . should not be allowed in regexp literals
     rhino       : false, // if the Rhino environment globals should be predefined
     scripturl   : true, // if script-targeted URLs should be tolerated
     shadow      : false, // if variable shadowing should be tolerated
+    smarttabs   : false, // if smarttabs should be tolerated (http://www.emacswiki.org/emacs/SmartTabs)
     strict      : true, // require the "use strict"; pragma
     sub         : false, // if all forms of subscript notation are tolerated
     supernew    : true, // if `new function () { ... };` and `new Object;` should be tolerated
     trailing    : false,// if trailing whitespace rules apply
     undef       : true, // if variables should be declared before used
+    unused      : true, // if variables should be always used
+    validthis   : true, // if 'this' inside a non-constructor function is valid. This is a function scoped option only.
+    withstmt    : true, // if with statements should be allowed
     white       : true, // if strict whitespace rules apply
+    worker      : true, // if Web Worker script symbols should be allowed
     wsh         : false, // if the Windows Scripting Host environment globals should be predefined
+
 
     'maxerr'    : 50,     //This option is used to specify maximum number of errors before JSHint stops processing your source. Default value is 50.
     'indent'    : 2     // The number of spaces used for indentation (default is 4)
